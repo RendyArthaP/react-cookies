b2da1fa7a911db9d5353ca9fed8b2067bfabf342
Rendy Artha <rendyartha312@gmail.com>
2021-06-05 22:58:17 +0700

    simple save user and token with usestate

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,51 @@
-import React from 'react'
+import Cookies from 'js-cookie'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [login, setLogin] = useState({
+    email: "",
+    password: ""
+  })
+  const [auth, setAuth] = useState(false)
+
+  const handleChange = (e) => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    Cookies.set("user", login)
+    Cookies.set("token", "1234568")
+    setAuth(true)
+  }
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleLogin}>
         <div>
           <label>
             Email
           </label>
-          <input type="text"/>
+          <input 
+            type="text"
+            value={login.email}
+            name="email"
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>
             Password
           </label>
-          <input type="password"/>
+          <input 
+            type="password"
+            value={login.password}
+            name="password"
+            onChange={handleChange}
+          />
         </div>
         <button type="submit">
           Submit
